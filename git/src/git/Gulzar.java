b6485bf81706a613d4714a8db05cbff03fc71c61
sdac-11 <sdac-11@sdac-11-PC>
2024-01-16 09:34:01 +0530

    t

--- git/src/git/Gulzar.java
+++ git/src/git/Gulzar.java
@@ -3,5 +3,7 @@ package git;
 public class Gulzar {
 public static void main(String[] args) {
 	System.out.println("this is gulzar");
+	System.out.println("this is vijay");
+	System.out.println("this is india");
 }
 }
